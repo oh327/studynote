bb3308187bd0974bad322cf73f3af2bebd76d0cb
hyeonJeong <ohj100414@gmail.com>
2022-01-24 20:43:06 +0900

    02-변수와-데이터-타입

--- 02-Javascript/01-Hello-Javascript/my-Hello-Javascript/HelloWorld.js
+++ 02-Javascript/01-Hello-Javascript/my-Hello-Javascript/HelloWorld.js
@@ -1,16 +1,16 @@
-"use strict"; // 엄격모드 적용 --> ES6가 등장하면서 함께 추가된 ES5의 변경사항을 활성화 시킴
+"use strict"; // 엄격모드 적용 --> ES6 호환기능만 사용하도록 명시
 
 // 메세지 그룹핑
 console.group("MyMessage1");
-console.log("안녕하세요. Javascript1");
-console.log("안녕하세요. Javascript2");
-console.log("안녕하세요. Javascript3");
+    console.log("안녕하세요. Javascript1");
+    console.log("안녕하세요. Javascript2");
+    console.log("안녕하세요. Javascript3");
 console.groupEnd();
 
 console.group("MyMessage2"); // 출력하는 내용을 그룹으로 묶음
-console.log("안녕하세요. Javascript4");
-console.group("MyMessage2-1");
-console.log("안녕하세요. Javascript5");
-console.log("안녕하세요. Javascript6");
-console.groupEnd();
+    console.log("안녕하세요. Javascript4");
+        console.group("MyMessage2-1"); // 그룹 안에 하위그룹 생성
+            console.log("안녕하세요. Javascript5");
+            console.log("안녕하세요. Javascript6");
+    console.groupEnd(); // 하위 그룹으로 묶기 끝
 console.groupEnd(); // 그룹으로 묶기 끝
