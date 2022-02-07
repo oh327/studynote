fe7a5878968da566c43edce038232c7a2e170637
hyeonJeong <ohj100414@gmail.com>
2022-02-07 21:52:46 +0900

    08-함수

---
+++ 02-Javascript/07-JSON/연습문제1.js
@@ -0,0 +1,13 @@
+const p = ['A', 'A', 'A', 'O', 'B', 'B', 'O', 'AB', 'AB', 'O'];
+const result = {
+    "A" : 0,
+    "B" : 0,
+    "AB" : 0,
+    "O" : 0
+};
+
+for (const b of p) {
+    result[b]++;
+}
+
+console.log(result);
---
+++ 02-Javascript/07-JSON/연습문제3-1.js
@@ -0,0 +1,19 @@
+covid19 = [
+    {date: '0125', active: 426}, 
+    {date: '0126', active: 343}, 
+    {date: '0127', active: 547}, 
+    {date: '0128', active: 490}, 
+    {date: '0129', active: 460}, 
+    {date: '0130', active: 443}, 
+    {date: '0131', active: 338}, 
+    {date: '0201', active: 299}
+]
+
+let sum = 0;
+
+for (const j of covid19) {
+    sum += j.active;
+}
+
+console.log("누적 확진자 수 : %d", sum);
+console.log("평균 확진자 수 : %d", sum / covid19.length);
